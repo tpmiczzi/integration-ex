689e5f42d077afad77f5746d5eb66187ae91687c
Pavlo Tula <pavlo.tula@thomsonreuters.com>
2019-07-22 14:09:20 +0300

    init

---
+++ src/test/java/com/tpm/integrationex/IntegrationExApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tpm.integrationex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrationExApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
